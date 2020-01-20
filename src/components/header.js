ae824c4c00209726b54f1a2f73198c094e5cea0b
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2020-01-20 02:23:50 -0500

    navbar responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,28 @@ import { Link } from "gatsby"
 import React from "react"
 import layoutStyles from "./layout.module.scss"
 import Image from "./image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import classNames from "classnames"
 
 const Header = () => {
+  const [isNavbarOpen, changeNavbar] = React.useState(false)
+
+  const inputEl = React.useRef(null)
+  const navSlide = () => {
+    changeNavbar(!isNavbarOpen)
+  }
+
+  let className = classNames(layoutStyles.navBar, {
+    [layoutStyles.navBarActive]: isNavbarOpen,
+  })
+
   return (
-    <div className={layoutStyles.header}>
+    <nav className={layoutStyles.header}>
       <div className={layoutStyles.logo}>
         <Image />
       </div>
-      <ul className={layoutStyles.navBar}>
+      <ul ref={inputEl} className={className}>
         <li>
           <Link to="#">Services</Link>
         </li>
@@ -26,7 +40,24 @@ const Header = () => {
           <Link to="#">Contact</Link>
         </li>
       </ul>
-    </div>
+      <div
+        role="button"
+        onClick={() => navSlide()}
+        onKeyDown={() => navSlide()}
+        tabIndex={0}
+        className={layoutStyles.burger}
+      >
+        <div className={layoutStyles.line1}></div>
+        <div className={layoutStyles.line2}></div>
+        <div className={layoutStyles.line3}></div>
+      </div>
+      {/* <div className={layoutStyles.check}>
+        <input type="checkbox" className={layoutStyles.checkInput} id="check" />
+        <div for="check">
+          <FontAwesomeIcon className={layoutStyles.checkbtn} icon={faBars} />
+        </div>
+      </div> */}
+    </nav>
   )
 }
 
