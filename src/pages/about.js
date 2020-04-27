bd915a714b961394ec27dfb7f4564490337acc15
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2020-04-27 01:58:50 -0300

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,31 +3,34 @@ import { Link } from "gatsby"
 import AboutStyles from "../components/AboutPage.module.scss"
 import Header from "../components/sub-pages-header"
 import Img from "gatsby-image"
+import classNames from "classnames"
 import { graphql, useStaticQuery } from "gatsby"
 import Footer from "../components/Footer"
 import "normalize.css"
 import "../styles/global.scss"
 
 const About = props => {
+  let className = classNames(AboutStyles.overlay, AboutStyles.color1)
+
   const data = useStaticQuery(graphql`
     query {
-      logo1: file(relativePath: { eq: "services7.jpg" }) {
+      logo1: file(relativePath: { eq: "national-team-event.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 640, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      logo2: file(relativePath: { eq: "score-system-tp.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 600, quality: 100) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      logo2: file(relativePath: { eq: "services9.jpg" }) {
+      logo3: file(relativePath: { eq: "hand-cap-tp.jpeg" }) {
         childImageSharp {
-          fluid(maxWidth: 600, quality: 100) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      logo3: file(relativePath: { eq: "services10.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 600, quality: 100) {
+          fluid(maxWidth: 640, quality: 100) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -46,19 +49,113 @@ const About = props => {
       />
       <div className={AboutStyles.wrapper}>
         <div className={AboutStyles.title}>
-          <h1>We're Team Poker®</h1>
+          <h1>We're TeamPoker®</h1>
         </div>
         <div className={AboutStyles.headlinetop}>
           <p>
             TeamPoker® is “The Patented Card Game Process” for the Tournament
             Poker Team Sports Model.
-            {/* <br /> Help us make Poker a Team Sport! Start a Club and Own the
-            Team! */}
           </p>
         </div>
         <div className={AboutStyles.content}>
           <div className={AboutStyles.container}>
             <div className={AboutStyles.image}>
+              <div
+                className={classNames(AboutStyles.overlay, AboutStyles.color1)}
+              ></div>
+              <Img
+                imgStyle={{}}
+                fluid={data.logo2.childImageSharp.fluid}
+                alt=""
+              />
+            </div>
+            <div
+              className={classNames(
+                AboutStyles.details,
+                AboutStyles.rightDetails
+              )}
+            >
+              <h1>Individual & Team Scoring</h1>
+              <p>
+                Tournament Directors can structure any poker game with
+                TeamPoker® so that both Individuals and Teams win a percentage
+                of the prize pool. Any split is possible from 0-100%, either
+                way.{" "}
+                <span className={AboutStyles.bold}>
+                  The inverse of the individual finish positions determines the
+                  Global Points awarded for Team Point Scores that determine
+                  team finish positions and payouts.
+                </span>
+                The team points ‘tie breaker’ is the highest Individual Finish
+                Position.
+              </p>
+              <p>
+                {" "}
+                Global Points allow Team Handicapping and are the root of
+                Standard Team Ranking for the WORLD POKER TEAM CHAMPIONSHIP™ -
+                SERIES Ranking.
+              </p>
+              <p>
+                {" "}
+                A second option for Team Scoring is{" "}
+                <span className={AboutStyles.bold}>Carry-The-Flog</span>, where
+                only the{" "}
+                <span className={AboutStyles.bold}>
+                  top individual finish positions determine team payout.
+                </span>{" "}
+                The top individual team player Carries-The-Flag for team payout,
+                and there is only one team payout per team.{" "}
+              </p>
+              <p>
+                <span className={AboutStyles.bold}>
+                  Both Individuals and Teams are Ranked by the TP Global Point
+                  System™
+                </span>{" "}
+                that determines finish positions, award, and can be used for the
+                basis of the optional Team Handicapping System.
+              </p>
+            </div>
+          </div>
+          <div className={AboutStyles.container}>
+            <div className={AboutStyles.details}>
+              <h1>TeamPoker® Seating Protocol</h1>
+              <p>
+                Tournament Directors structure and publish their Team Seating
+                Requirements along with the typical tournament protocols of time
+                periods, blind structure, etc. for running TeamPoker®
+                Tournaments.
+              </p>
+              <p>
+                {" "}
+                <span className={AboutStyles.bold}>TP NON-CONTACT:</span> The
+                preferred default method of seating is No-Contact, where
+                teammates are not allowed together with opponents on the same
+                table in tournaments. Teammates may play together or heads up
+                during tournament table consolidation, but never on same table
+                with opponents.{" "}
+              </p>
+              <p>
+                <span className={AboutStyles.bold}>TP FULL-CONTACT:</span> The
+                Full-Contact rule is the Standard Random Seating of Players and
+                allows teammates on the same table with opponents. Considered a
+                Pro-Seating Option, there is technically no possibility of
+                collusion with Full-Contact as teammates are known and
+                identified.
+              </p>
+              <p>
+                {" "}
+                <span className={AboutStyles.bold}>
+                  TP HYBRID SEATING PROTOCOL:
+                </span>{" "}
+                Using TeamPoker® you can switch from Non-Contact to Full Contact
+                at any time and it can be used at the end of tournament, as
+                example, to conserve tables.
+              </p>
+            </div>
+            <div className={AboutStyles.image}>
+              <div
+                className={classNames(AboutStyles.overlay, AboutStyles.color2)}
+              ></div>
               <Img
                 style={{}}
                 imgStyle={{}}
@@ -66,43 +163,12 @@ const About = props => {
                 alt=""
               />
             </div>
-            <div className={AboutStyles.details}>
-              <h3>Individual & Team Scoring</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-          </div>
-          <div className={AboutStyles.container}>
-            <div className={AboutStyles.details}>
-              <h3>TeamPoker® Seating Protocol</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
-            <div className={AboutStyles.image}>
-              <Img
-                style={{}}
-                imgStyle={{}}
-                fluid={data.logo2.childImageSharp.fluid}
-                alt=""
-              />
-            </div>
           </div>
           <div className={AboutStyles.container}>
             <div className={AboutStyles.image}>
+              <div
+                className={classNames(AboutStyles.overlay, AboutStyles.color3)}
+              ></div>
               <Img
                 style={{}}
                 imgStyle={{}}
@@ -110,19 +176,53 @@ const About = props => {
                 alt=""
               />
             </div>
-            <div className={AboutStyles.details}>
-              <h3>TeamPoker® Handicap</h3>
+            <div
+              className={classNames(
+                AboutStyles.details,
+                AboutStyles.rightDetails
+              )}
+            >
+              <h1>TeamPoker® Handicap</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                TeamPoker® allows the Tournament Director to structure any sized
+                team event, and, team size can vary in the same tournament using
+                the TP Handicap.{" "}
+              </p>
+              <p>
+                <span className={AboutStyles.bold}>
+                  The Handicap is the ratio of the max team size allowed divided
+                  by the actual size within the minimum allowed by tournament
+                  structure. This ratio is multiplied by the Teams Global Points
+                  (inverse of finish position count);{" "}
+                </span>
+                and, Team Points are compared for Team Finish Position and Prize
+                Pool Payout.{" "}
+              </p>
+              <p>
+                <span className={AboutStyles.bold}>
+                  The team score tie breaker is always the team with the highest
+                  Individual Finish Position.
+                </span>
+              </p>
+              <p>
+                <span className={AboutStyles.bold}>
+                  {" "}
+                  The TeamPoker® Handicap and Scoring System is the new paradigm
+                  in tournament poker where Individuals and Teams can handicap
+                  their skills (Reverse Handicap) and compete for larger portion
+                  of the prize pool.
+                </span>
               </p>
             </div>
           </div>
+        </div>
+        <div className={AboutStyles.headline}>
+          <p>Help us make Poker a Team Sport!</p>
+        </div>
+        <div className={AboutStyles.contact}>
+          <Link to="#" className={AboutStyles.button}>
+            Contact Us to Learn More
+          </Link>
         </div>
       </div>
       <Footer />
